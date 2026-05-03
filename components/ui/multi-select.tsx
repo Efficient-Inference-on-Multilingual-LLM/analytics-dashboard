7a119634edfda21573ad432469092854ed9acc9e
Tansil011019 <wilsontansil@gmail.com>
2026-05-03 21:22:00 +0700

    feat: add multi selection

---
+++ components/ui/multi-select.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Combobox, ComboboxChip, ComboboxChips, ComboboxChipsInput, ComboboxContent, ComboboxEmpty, ComboboxItem, ComboboxList, ComboboxValue, useComboboxAnchor } from './combobox'
+import { Label } from './label';
+
+interface MultiSelectProps <T extends {value: string, label: string}> {
+  frameworks: T[];
+  className?: string;
+  label?: string;
+}
+
+function MultiSelect<T extends {value: string, label: string}>({ frameworks, className, label }: MultiSelectProps<T>) {
+  const anchor = useComboboxAnchor();
+
+  return (
+    <div className={className}>
+      {label && (
+        <Label className='text-sm px-1'>
+          {label}
+        </Label>
+      )}
+      <Combobox
+          multiple
+          autoHighlight
+          items={frameworks}
+      >
+        <ComboboxChips ref={anchor} className="w-full">
+          <ComboboxValue>
+          {(items: T[]) => {
+            return (
+              <React.Fragment>
+                {items.map((item: T) => (
+                  <ComboboxChip key={item.value}>
+                    {item.label}
+                  </ComboboxChip>
+                ))}
+                <ComboboxChipsInput placeholder={items.length === 0 ? "Select model to compare" : ""} />
+              </React.Fragment>
+            )
+          }}
+          </ComboboxValue>
+        </ComboboxChips>
+        <ComboboxContent anchor={anchor}>
+          <ComboboxEmpty>No frameworks found.</ComboboxEmpty>
+          <ComboboxList>
+            {(item: T) => (
+              <ComboboxItem key={item.value} value={item}>
+                {item.label}
+              </ComboboxItem>
+            )}
+          </ComboboxList>
+        </ComboboxContent>
+      </Combobox>
+    </div>
+  )
+}
+
+export default MultiSelect
