7a78c6a8952fca08281a61112097791f3a419024
Tansil011019 <wilsontansil@gmail.com>
2026-05-04 16:26:46 +0700

    refactor: method selector clearable

--- components/filter/method-selector.tsx
+++ components/filter/method-selector.tsx
@@ -1,33 +1,37 @@
 import React from "react";
 import {
-  Select,
-  SelectValue,
-  SelectTrigger,
-  SelectItem,
-  SelectContent,
-  SelectGroup,
-} from "../ui/select";
+  Combobox,
+  ComboboxContent,
+  ComboboxEmpty,
+  ComboboxInput,
+  ComboboxItem,
+  ComboboxList,
+} from "../ui/combobox";
 import { Label } from "../ui/label";
 import { METHODS } from "@/types/constant";
 
 const MethodSelector = () => {
+  type MethodOption = (typeof METHODS)[number];
   return (
     <div className="flex flex-col w-full">
       <Label className="text-sm px-1">Method</Label>
-      <Select>
-        <SelectTrigger className="w-full">
-          <SelectValue placeholder="Select a method" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            {METHODS.map((method) => (
-              <SelectItem key={method.value} value={method.value}>
-                {method.label}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
+      <Combobox
+        items={METHODS}
+        itemToStringLabel={(item: MethodOption) => item.label}
+        itemToStringValue={(item: MethodOption) => item.value}
+      >
+        <ComboboxInput placeholder="Select a method" showClear />
+        <ComboboxContent>
+          <ComboboxEmpty>No items found.</ComboboxEmpty>
+          <ComboboxList>
+            {(item) => (
+              <ComboboxItem key={item.value} value={item}>
+                {item.label}
+              </ComboboxItem>
+            )}
+          </ComboboxList>
+        </ComboboxContent>
+      </Combobox>
     </div>
   );
 };
